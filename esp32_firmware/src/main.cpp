e50b06d2a88ffe81e6e7fe1a96817e150d3ce141
DangTran-hub <dangtranmyha03092003@gmail.com>
2025-11-29 23:27:01 +0700

    config file ini

--- esp32_firmware/src/main.cpp
+++ esp32_firmware/src/main.cpp
@@ -1,20 +1,10 @@
 #include <Arduino.h>
+void setup()
+{
 
-#define BUTTON_PIN 16
-#define LED_PIN 17
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);  // Dùng điện trở kéo xuống nội bộ
-  pinMode(LED_PIN, OUTPUT);
 }
-
-void loop() {
-  if (digitalRead(BUTTON_PIN) == HIGH) { // Khi nhấn
-    Serial.println("LED ON");
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    Serial.println("LED OFF");
-    digitalWrite(LED_PIN, LOW);
-  }
+ 
+void loop()
+{
+  
 }
